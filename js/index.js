1aa425813aaa5ba10bdb98d6dc0a555c93ef29bf
sam <sam789654123@gmail.com>
2020-09-29 21:53:49 +0800

    隨機陣列控制CSS組件 + 駝峰命名更改 + 狀態碼控制選取區塊
    Signed-off-by: sam <sam789654123@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,98 +1,45 @@
 (function () {
   window.addEventListener('load', function () {
-    // 宣告與CSS相通物件，物品->物品等級->物品名稱->'class'
-    // 隨機用陣列，數量100個
-    let wolf_ary = [];
-    // 機率區 40% 30% 20% 10%
-    let wolf_amount100 = 100;
-    let wolf_amount40 = (100 * 40) / 100;
-    let wolf_amount30 = (100 * 30) / 100;
-    let wolf_amount20 = (100 * 20) / 100;
-    let wolf_amount10 = (100 * 10) / 100;
-    if (
-      wolf_amount100 ===
-      wolf_amount40 + wolf_amount30 + wolf_amount20 + wolf_amount10
-    ) {
-      for (let i = 1; i <= wolf_amount40; i++) {
-        wolf_ary.push('grass01');
-      }
-      for (let i = 1; i <= wolf_amount30; i++) {
-        wolf_ary.push('grass02');
-      }
-      for (let i = 1; i <= wolf_amount20; i++) {
-        wolf_ary.push('grass03');
-      }
-      for (let i = 1; i <= wolf_amount10; i++) {
-        wolf_ary.push('grass04');
-      }
+    // 隨機用陣列 10筆資料，對應下方randomName 隨機
+    let propsAry = [
+      'grass1',
+      'grass1',
+      'grass1',
+      'grass1',
+      'grass1',
+      'grass2',
+      'grass2',
+      'grass2',
+      'grass3',
+      'grass4',
+    ];
+
+    const clickItem = document.getElementsByName('click_item'); // 用來選取地板區塊
+    const randomItem = document.querySelector('.random_item'); //用來選取左側單一框
+    let randomName = propsAry[Math.floor(Math.random() * 10)]; // 隨機物品 目前 10筆 ，需與陣列propsAry對應相同筆數
+    // 若隨機有值則將'使用道具' 放進左側單一框框，否則給一個預設地板
+    if (randomName) {
+      randomItem.classList.add(randomName);
     } else {
-      console.log('機率總和大於100%');
+      randomItem.classList.add(grass1);
     }
 
-    console.log(wolf_ary);
-    /* 點擊新增道具 */
-    const g_click = document.getElementsByName('click_item');
-    const random_item = document.querySelector('.random_item');
-    // 隨機物品 目前 grass, wolf
-    let random_name = Math.floor(Math.random() * 2) + 1;
-    // 隨機物品等級 目前 1~4
-    let random_level = Math.floor(Math.random() * 4) + 1;
-    //  選取相對應物品
-    if (random_name) {
-      switch (random_name) {
-        case 1:
-          random_name = levelObj.grass;
-          break;
-        case 2:
-          random_name = levelObj.wolf;
-          break;
-      }
-    }
-    //  選取相對應物品等級
-    if (random_name && random_level) {
-      switch (random_level) {
-        case 1:
-          random_level = random_name.level1.name;
-          break;
-        case 2:
-          random_level = random_name.level2.name;
-          break;
-        case 3:
-          random_level = random_name.level3.name;
-          break;
-        case 4:
-          random_level = random_name.level4.name;
-          break;
-      }
-    }
-
-    if (random_name && random_level) {
-      // 將'使用道具' 放進左側單一框框
-      random_item.classList.add(random_level);
-    } else {
-      random_item.classList.add(levelObj.grass.level1.name);
-    }
-
-    // 將隨機到的道具經由點擊放進地板內
-    // g_click.forEach(function (value) {
-    //   value.addEventListener('click', function () {
-    //     // 切割class 查看目前有幾個class
-    //     let class_grass = value.className.split(' ')
-    //     // 判斷如果有第二個class則不執行下面動作
-    //     if (!class_grass || class_grass.length <= 1) {
-    //       // 給地板道具
-    //       value.classList.add('grass0' + random)
-    //       // 清除'使用道具'
-    //       random_item.classList.remove('grass0' + random)
-
-    //       // 隨機一個新的'使用道具' 1~4階
-    //       random = Math.floor(Math.random() * 4) + 1
-    //       // 將新'使用道具' 放進左側單一框框
-    //       random_item.classList.add('grass0' + random)
-    //     }
-    //   })
-    // })
+    //將隨機到的道具經由點擊放進地板內
+    clickItem.forEach(function (value) {
+      value.addEventListener('click', function () {
+        // 判斷data-status = 0 且 有隨機道具時執行下面程式
+        if (value.dataset.status == 0 && randomName) {
+          value.classList.add(randomName); // 給地板道具
+          randomItem.classList.remove(randomName); // 清除左側單一框'使用道具'
+          randomName = propsAry[Math.floor(Math.random() * 10)]; // 隨機物品一個新的'使用道具' 目前 10筆 ，需與陣列propsAry對應相同筆數
+          randomItem.classList.add(randomName); // 將新'使用道具' 放進左側單一框框
+          value.dataset.status = 1; // 將狀態碼改成一，避免重複點擊
+        }
+      });
+    });
   });
+
+  // 宣告與CSS相通物件，物品->物品等級->物品名稱->'class'
   let levelObj = {
     grave: {
       level1: {
